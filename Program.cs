3dbf666bb70c6f5c34d31c4214bb56658d69e02f
jonathanMelly <jonathan.melly@gmail.com>
2020-02-24 15:38:33 +0100

    Ajout d'une saisie utilisateur avant la fermeture du programme

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace TestGitCreatedFromVS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
