7cab9a2f7555feb38b736b1c72f84824c85902d9
diurnambule <jonathan.melly@gmail.com>
2020-02-24 15:35:07 +0100

    ajout des descripteurs de projet C# et classe de base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGitCreatedFromVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
